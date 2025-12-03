c8bfe1f7980e4bcb4fe008046cfc3945e40b6a0e
Dmytruk Serhii <dmitruksl92@gmail.com>
2025-12-03 19:08:01 +0200

    First attempt

--- src/components/Tabs/Tabs.jsx
+++ src/components/Tabs/Tabs.jsx
@@ -1,1 +1,25 @@
-export const Tabs = () => {};
+export const Tabs = ({ tabs, activeTabId, onTabSelected }) => {
+  return (
+    <ul>
+      {tabs.map(tab => {
+        return (
+          <li
+            data-cy="Tab"
+            className={tab.id === activeTabId ? 'is-active' : ''}
+            key={tab.id}
+          >
+            <a
+              href={`#${tab.id}`}
+              data-cy="TabLink"
+              onClick={() => {
+                onTabSelected(tab.id);
+              }}
+            >
+              {tab.title}
+            </a>
+          </li>
+        );
+      })}
+    </ul>
+  );
+};
